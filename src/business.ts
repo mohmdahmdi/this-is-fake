25b6208bf821d40e399789dce2e0ca71d2765f48
mohamad ahmadi <mohammad.pr285@gmail.com>
2025-10-02 12:21:06 +0330

    feat: enhance business generation with detailed attributes and randomization logic

--- src/business.ts
+++ src/business.ts
@@ -3,27 +3,48 @@ import { writeCSV } from "./utils/faker-helpers";
 import districts from "./constants/districts";
 import { readCsv } from "./utils/read-csv";
 import { Users } from "./user";
+import businessDescription from "./utils/business-description";
+import categories from "./constants/categories.json";
 
 async function generateBusinesses(count: number) {
   let users = await readCsv<Users>("../dist/csv/user.csv");
 
-  // const locations = await readCsv<Location>("../dist/csv/location.csv");
+  const locations = await readCsv<Location>("../dist/csv/location.csv");
 
   return Array.from({ length: count }, () => {
-    const owner_id = users[Math.floor(Math.random() * users.length)].id;
-    users.length > 5 && users.splice(users.findIndex(predicate => predicate.id === owner_id))
-    const name = "";
-    console.log(users);
+    const user = users[Math.floor(Math.random() * users.length) - 1];
+    const owner_id = user.id;
+    const business_type =
+      Object.keys(categories)[
+        Math.floor(Math.random() * Object.keys(categories).length)
+      ];
+    const name = ``;
+    const location_index = Math.floor(Math.random() * locations.length);
+    const location_id = locations[location_index];
+    locations.splice(location_index, 1);
+    const phone = faker.phone.number({ style: "international" });
+
     return {
       id: faker.string.uuid(),
       owner_id,
       name,
-      description: "",
-      city: faker.location.city(),
-      district: faker.helpers.arrayElement(districts),
-      address: `کوچه ${Math.floor(Math.random() * 50)}`,
-      latitude: faker.location.latitude({ min: 25, max: 40 }),
-      longitude: faker.location.longitude({ min: 44, max: 63 }),
+      description: businessDescription((business_type as keyof B) || undefined),
+      logo: faker.internet.url(),
+      cover_image: faker.internet.url(),
+      location_id,
+      business_type_id: business_type,
+      phone,
+      email: Math.random() > 0.33 ? faker.internet.email() : null,
+      website:
+        Math.random() > 0.9 ? faker.internet.url({ protocol: "http" }) : null,
+      instagram:
+        Math.random() > 0.6
+          ? `@${faker.internet.username().toLowerCase()}`
+          : null,
+      whatsapp: Math.random() > 0.8 ? phone : null,
+      rating: (Math.random() * 5).toPrecision(2),
+      is_verified: Math.random() > 0.1,
+      is_active: Math.random() > 0.2,
     };
   });
 }
